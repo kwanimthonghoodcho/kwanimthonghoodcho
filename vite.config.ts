c3964129b7e1402f841a017dfcd23142dae3c1a9
kwanimthonghoodcho <kwanimthonghoodcho@gmail.com>
2025-01-10 04:15:28 +0800

    base: '/'

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kwanimthonghoodcho/', // GitHub Pages base path
+  base: '/', // GitHub Pages base path
   build: {
     outDir: 'dist', // Output to 'build' directory for gh-pages
   },
