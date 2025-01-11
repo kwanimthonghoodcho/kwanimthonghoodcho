777e395933bf0df6a8d5d8579bb46b3d69443150
kwanimthonghoodcho <kwanimthonghoodcho@gmail.com>
2025-01-11 15:35:23 +0800

    chunti elaboration

--- src/pages/Chunti.tsx
+++ src/pages/Chunti.tsx
@@ -4,12 +4,16 @@ const Chunti = () => {
     return (
       <section>
         <img src={chuntiPhoto} className="chunti" alt="Chunti Bodhisattva" />
+        
+        <p>Chunti Bodhisattva is known as the Chunti Buddha Mother of Seventy Million Buddhas. The 18 arms represent the 18 merits attaining Buddhahood.</p>
         <p>
           The Chunti Mantra is a powerful chant believed to purify the mind and heart.
           Chanting it helps to eliminate obstacles and cultivate compassion.
         </p>
         <p>
-          準提神咒: 唵，折戾主戾，准提娑婆诃
+          Cunti long mantra: Nan-mo-sa-due-nan, san-miao-san-pu-tuo, ju-zhi-nan da-zhi-ta, om zhe-li-zhu-li, chun-ti so-ha
+          Cunti heart mantra: om zhe-li-zhu-li, chun-ti so-ha
+          準提神咒: 南无飒哆喃，三藐三菩陀，俱胝喃，怛侄他，唵，折戾主戾，准提娑婆诃
         </p>
       </section>
     );
